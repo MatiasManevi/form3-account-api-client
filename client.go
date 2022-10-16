0d0494e4b76d379484c9d66a5a3bb06c1894e6b1
Matias Manevi <manevimatias@gmail.com>
2022-10-16 02:16:25 -0300

    Add common doRequest func

--- client.go
+++ client.go
@@ -3,7 +3,11 @@ package form3Client
 import (
     "net/http"
     "time"
-	"os"
+    "fmt"
+	"encoding/json"
+	"errors"
+	"context"
+	"io"
 )
 
 type Client struct {
@@ -19,7 +23,8 @@ type errorResponse struct {
 }
 
 func NewClient() *Client {
-	host := os.Getenv("API_HOST")
+	// host := os.Getenv("API_HOST")
+	host := "http://localhost:8080/v1/"
 	client := &http.Client{
 		Timeout: time.Minute,
 	}
@@ -27,4 +32,53 @@ func NewClient() *Client {
         Host: host,
         HTTPClient: client,
     }
+}
+
+func (c *Client) doRequest(method, endpoint string, v interface{}, data io.Reader) error {
+	// HTTP request creation
+	baseURL := fmt.Sprintf("%s/%s", c.Host, endpoint)
+	req, err := http.NewRequest(method, baseURL, data)
+	// Handle HTTP request creation errors
+	if err != nil {
+        return err
+    }
+
+	// Use context on request to control reuqest deadline
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	req = req.WithContext(ctx)
+
+	// Setting common headers
+    req.Header.Set("Content-Type", "application/json; charset=utf-8")
+    req.Header.Set("Accept", "application/json; charset=utf-8")
+
+	// Run request
+	res, err := c.HTTPClient.Do(req)
+	// Handle HTTP request errors
+    if err != nil {
+        return err
+    }
+    defer res.Body.Close()
+
+	// Checking for errors in response status code
+    if res.StatusCode != http.StatusOK {
+        var errRes errorResponse
+        if err = json.NewDecoder(res.Body).Decode(&errRes); err != nil {
+			// Error response couldn't be decoded
+			return fmt.Errorf("unknown error, status code: %d", res.StatusCode)
+		}
+		
+		return errors.New(errRes.ErrorMessage)
+    }
+
+    response := successResponse{
+        Data: v,
+    }
+
+	// Checking for errors in response decoding
+    if err = json.NewDecoder(res.Body).Decode(&response); err != nil {
+        return err
+    }
+	
+	return nil
 }
